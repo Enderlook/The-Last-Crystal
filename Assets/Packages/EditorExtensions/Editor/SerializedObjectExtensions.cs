a7b375b8245721c2eb8935c946be1cb4d363cb98
Ender Look <enderlook@gmail.com>
2019-09-07 23:59:49 -0300

    Add ToggleableField
    Add ToggleabeField from EditorExtensions to SerializedObjectExtensions. Apply to EditorExtensions.

--- Assets/Packages/EditorExtensions/Editor/SerializedObjectExtensions.cs
+++ Assets/Packages/EditorExtensions/Editor/SerializedObjectExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 
 public static class SerializedObjectExtensions
 {
@@ -104,6 +106,131 @@ public static class SerializedObjectExtensions
             EditorGUI.indentLevel++;
             action();
             EditorGUI.indentLevel--;
+        }
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <typeparam name="T">Type of return value by <paramref name="func"/>.</typeparam>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="field">Field to store the changes made by the function.</param>
+    /// <param name="toShowField">Function which creates the field on Unity Inspector.</param>
+    /// <param name="confirmationVariable">Boolean variable to store the toggle check.</param>
+    /// <param name="confirmationLabel">Text of label.</param>
+    /// <param name="confirmationTooltip">Text of tooltip.</param>
+    /// <param name="reasonForUndo">Reason used when save undo operation.<br>
+    /// If <see langword="null"/>, <paramref name="confirmationLabel"/> will be used instead.<br>
+    /// To this it's appended " Checkbox." or " Change value." depending which action it was done.</param>
+    public static void ToggleableField<T>(this SerializedObject source, ref T field, Func<T> toShowField, ref bool confirmationVariable, string confirmationLabel, string confirmationTooltip = "", string reasonForUndo = null)
+    {
+        source.ToggleableField(ref field, toShowField, ref confirmationVariable, new GUIContent(confirmationLabel, confirmationTooltip), reasonForUndo ?? confirmationLabel);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <typeparam name="T">Type of return value by <paramref name="func"/>.</typeparam>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="field">Field to store the changes made by the function.</param>
+    /// <param name="toShowfield">Function which creates the field on Unity Inspector.</param>
+    /// <param name="confirmationVariable">Boolean variable to store the toggle check.</param>
+    /// <param name="confirmationContent">Text of label.</param>
+    /// <param name="reasonForUndo">Reason used when save undo operation.</br>
+    /// If <see langword="null"/>, <c><paramref name="confirmationContent"/>.text</c> will be used instead.<br>
+    /// To this it's appended " Checkbox." or " Change value." depending which action it was done.</param>
+    public static void ToggleableField<T>(this SerializedObject source, ref T field, Func<T> toShowfield, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo)
+    {
+        // https://answers.unity.com/questions/192895/hideshow-properties-dynamically-in-inspector.html
+        // https://www.reddit.com/r/Unity3D/comments/45bjwc/tooltip_on_custom_inspectorproperties/
+
+        reasonForUndo = reasonForUndo ?? confirmationContent.text;
+
+        bool toggleValue = confirmationVariable;
+        source.ChangeCheck(() => GUILayout.Toggle(toggleValue, confirmationContent), ref confirmationVariable, $"{reasonForUndo}. Checkbox.");
+
+        source.DrawFieldIfConfirmed(ref field, toShowfield, confirmationVariable, reasonForUndo);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="serializedProperty"><see cref="SerializedProperty"/> to show in the inspector./param>
+    /// <param name="confirmationVariable">Boolean variable to store the toggle check.</param>
+    /// <param name="confirmationContent">Text of label.</param>
+    /// <param name="reasonForUndo">Reason used when save undo operation. To this it's appended " Checkbox.".</br>
+    /// If <see langword="null"/>, <c><paramref name="confirmationContent"/>.text</c> will be used instead.</param>
+    /// <param name="includeChildren"/>If <see langword="true"/> the property including children is drawn.</param>
+    public static void ToggleableField(this SerializedObject source, SerializedProperty serializedProperty, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo, bool includeChildren = false)
+    {
+        reasonForUndo = reasonForUndo ?? confirmationContent.text;
+
+        bool toggleValue = confirmationVariable;
+        source.ChangeCheck(() => GUILayout.Toggle(toggleValue, confirmationContent), ref confirmationVariable, $"{reasonForUndo}. Checkbox.");
+        source.DrawFieldIfConfirmed(serializedProperty, includeChildren, confirmationVariable);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="serializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector./param>
+    /// <param name="confirmationVariable">Boolean variable to store the toggle check.</param>
+    /// <param name="confirmationContent">Text of label.</param>
+    /// <param name="reasonForUndo">Reason used when save undo operation. To this it's appended " Checkbox.".</br>
+    /// If <see langword="null"/>, <c><paramref name="confirmationContent"/>.text</c> will be used instead.</param>
+    /// <param name="includeChildren"/>If <see langword="true"/> the property including children is drawn.</param>
+    public static void ToggleableField(this SerializedObject source, string serializedProperty, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo, bool includeChildren = false)
+    {
+        reasonForUndo = reasonForUndo ?? confirmationContent.text;
+
+        bool toggleValue = confirmationVariable;
+        source.ChangeCheck(() => GUILayout.Toggle(toggleValue, confirmationContent), ref confirmationVariable, $"{reasonForUndo}. Checkbox.");
+        source.DrawFieldIfConfirmed(serializedProperty, includeChildren, confirmationVariable);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="serializedProperty"><see cref="SerializedProperty"/> to show in the inspector./param>
+    /// <param name="booleanSerializedProperty"><see cref="SerializedProperty"/> to show in the inspector as confirmation checkbox./param>
+    /// <param name="includeChildren"/>If <see langword="true"/> the <paramref name="serializedProperty"/> including children is drawn.</param>
+    public static void ToggleableField(this SerializedObject source, SerializedProperty serializedProperty, SerializedProperty booleanSerializedProperty, bool includeChildren = false)
+    {
+        source.PropertyFieldAutoSave(booleanSerializedProperty);
+        source.DrawFieldIfConfirmed(serializedProperty, includeChildren, booleanSerializedProperty.boolValue);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="serializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector./param>
+    /// <param name="booleanSerializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector as confirmation checkbox./param>
+    /// <param name="includeChildren"/>If <see langword="true"/> the <paramref name="serializedProperty"/> including children is drawn.</param>
+    public static void ToggleableField(this SerializedObject source, string serializedProperty, string booleanSerializedProperty, bool includeChildren = false)
+    {
+        source.ToggleableField(source.FindProperty(serializedProperty), source.FindProperty(booleanSerializedProperty), includeChildren);
+    }
+
+    /// <summary>
+    /// Generate a toggleable button to hide or show a certain field, which is also created by this method.
+    /// </summary>
+    /// <param name="source">Instance where its executed this method.</param>
+    /// <param name="serializedProperty"Name of <see cref="SerializedProperty"/> to show in the inspector.<br>
+    /// This field must have a <see cref="HasConfirmationFieldAttribute"/>.</param>
+    /// <param name="includeChildren"/>If <see langword="true"/> the <paramref name="serializedProperty"/> including children is drawn.</param>
+    public static void ToggleableField(this SerializedObject source, string serializedProperty, bool includeChildren = false)
+    {
+        Type type = source.targetObject.GetType();
+        HasConfirmationFieldAttribute attribute = type.GetField(serializedProperty).GetCustomAttribute(typeof(HasConfirmationFieldAttribute)) as HasConfirmationFieldAttribute;
+        if (attribute == null)
+            throw new Exception($"The {type}.{serializedProperty} field must have the attribute {nameof(HasConfirmationFieldAttribute)}.");
+        else
+        {
+            source.ToggleableField(serializedProperty, attribute.ConfirmFieldName, includeChildren);
         }
     }
 }
