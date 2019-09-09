7c1a04b396cf362d6a2769d1ad4300e63f0f7cec
Ender Look <enderlook@gmail.com>
2019-09-09 15:42:19 -0300

    Add regenerate button in Unity Inspector

---
+++ Assets/Scripts/Navigation/Editor/NavigationEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Navigation))]
+public class NavigationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Navigation navigation = (Navigation)target;
+
+        if (GUILayout.Button("Regenerate Grid"))
+            navigation.GenerateGrid();
+    }
+}
