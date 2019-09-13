60e0b6eaf5cac1e95dad7a5ed759f22f423a87a9
Ender Look <enderlook@gmail.com>
2019-09-13 19:57:03 -0300

    Move Node and Connection classes to their own files

---
+++ Assets/Scripts/Navigation/Connection.cs
@@ -0,0 +1,32 @@
+﻿using Navigation;
+#if UNITY_EDITOR
+using UnityEngine;
+#endif
+
+namespace Navigation
+{
+    public class Connection
+    {
+        public Node start;
+        public Node end;
+        public float Distance => Vector2.Distance(start.position, end.position);
+        public bool IsActive { get; private set; }
+
+        public Connection(Node start, Node end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public void SetActive(bool active) => IsActive = active;
+
+#if UNITY_EDITOR
+        public void DrawConnection(Color active, Color inactive) => DrawConnection(IsActive ? active : inactive);
+        public void DrawConnection(Color color)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawLine(start.position, end.position);
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Navigation/Node.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Navigation
+{
+    public class Node
+    {
+        public Vector2 position;
+        public Connection[] connections;
+        public bool IsActive { get; private set; }
+
+        public Node(Vector2 position)
+        {
+            this.position = position;
+            connections = new Connection[8];
+        }
+
+        public void SetActive(bool actived) => IsActive = actived;
+
+#if UNITY_EDITOR
+        private float nodeDrawSize = 0.05f;
+        public void DrawNode(Color active, Color inactive) => DrawNode(IsActive ? active : inactive);
+        public void DrawNode(Color color)
+        {
+            Handles.color = color;
+            Handles.DrawSolidDisc(position, Vector3.forward, nodeDrawSize);
+        }
+        public void DrawConnections(Color active, Color inactive)
+        {
+            foreach (Connection connection in connections)
+            {
+                connection?.DrawConnection(active, inactive);
+            }
+        }
+        public void DrawConnections(Color color)
+        {
+            foreach (Connection connection in connections)
+            {
+                connection?.DrawConnection(color);
+            }
+        }
+#endif
+    }
+}
