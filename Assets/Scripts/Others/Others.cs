da39131d7a6c0e0acf9f6d4e45c0578d4785eb9d
Ender Look <enderlook@gmail.com>
2019-09-17 23:15:02 -0300

    Move StringExtensions to its own file

--- Assets/Scripts/Others/Others.cs
+++ Assets/Scripts/Others/Others.cs
@@ -35,18 +35,3 @@ public static class OthersExtensions
         value = tuple.Value;
     }
 }
-
-public static class StringExtensions
-{
-    /// <summary>
-    /// Return an string which first character is to upper case.
-    /// </summary>
-    /// <param name="source"></param>
-    /// <returns></returns>
-    public static string FirstCharToUpper(this string source)
-    {
-        if (source == null) throw new System.ArgumentNullException(nameof(source));
-
-        return source.Length > 1 ? char.ToUpper(source[0]) + source.Substring(1) : source.ToUpper();
-    }
-}
