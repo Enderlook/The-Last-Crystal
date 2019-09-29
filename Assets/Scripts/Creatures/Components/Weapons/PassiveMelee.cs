94d8ae5cf9e52abc9f7e2a1b833913b96e172858
Enderlook <enderlook@gmail.com>
2019-09-29 09:18:02 -0300

    Add PassiveMelee

---
+++ Assets/Scripts/Creatures/Components/Weapons/PassiveMelee.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace CreaturesAddons
+{
+    public class PassiveMelee : MonoBehaviour, IAwake, IDamageOnTouch
+    {
+        [Header("Configuration")]
+        [SerializeField, Tooltip("Damage on hit.")]
+        private float damage = 1;
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Push strength on hit.")]
+        private float pushStrength;
+#pragma warning restore CS0649
+
+
+        private Transform thisTransform;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Type Safety", "UNT0006:Incorrect message signature", Justification = "This isn't Unity method.")]
+        void IAwake.Awake(Creature creature) => thisTransform = creature.Transform;
+
+        void IDamageOnTouch.ProduceDamage(Creature victim)
+        {
+            victim.TakeDamage(damage);
+            victim.Push(thisTransform.position, pushStrength, Creature.PushMode.Local);
+        }
+    }
+
+    public interface IDamageOnTouch
+    {
+        /// <summary>
+        /// Produce damage
+        /// </summary>
+        /// <param name="creature"></param>
+        void ProduceDamage(Creature victim);
+    }
+}
