be7990a637d96b989a5e64ec1ccfbfc2f5f29e7b
Enderlook <enderlook@gmail.com>
2019-10-01 11:17:43 -0300

    Refactor Clockwork
    Refactor Clockwork. Add cycle counter.

--- Assets/Scripts/Creatures/Components/Weapons/Weapon.cs
+++ Assets/Scripts/Creatures/Components/Weapons/Weapon.cs
@@ -19,13 +19,18 @@ namespace CreaturesAddons
         public float TotalCooldown => ((IClockWork)clockwork).TotalCooldown;
         public float CooldownPercent => ((IClockWork)clockwork).CooldownPercent;
         public bool IsReady => ((IClockWork)clockwork).IsReady;
+        public int TotalCycles => ((IClockWork)clockwork).TotalCycles;
+        public int RemainingCycles => ((IClockWork)clockwork).RemainingCycles;
+        public bool IsEndlessLoop => ((IClockWork)clockwork).IsEndlessLoop;
+        public bool IsEnabled => ((IClockWork)clockwork).IsEnabled;
         public void Execute() => ((IClockWork)clockwork).Execute();
         public bool Recharge(float deltaTime) => ((IClockWork)clockwork).Recharge(deltaTime);
         public void ResetCooldown() => ((IClockWork)clockwork).ResetCooldown();
         public bool TryExecute(float deltaTime = 0) => ((IClockWork)clockwork).TryExecute(deltaTime);
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Type Safety", "UNT0006:Incorrect message signature", Justification = "This isn't Unity method.")]
         public void UpdateBehaviour(float deltaTime) => ((IClockWork)clockwork).UpdateBehaviour(deltaTime);
+        public void ResetCycles() => ((IClockWork)clockwork).ResetCycles();
+        public void ResetCycles(int newCycles) => ((IClockWork)clockwork).ResetCycles(newCycles);
+        public void ResetCooldown(float newCooldownTime) => ((IClockWork)clockwork).ResetCooldown(newCooldownTime);
     }
 
     public interface ITakeDamage
