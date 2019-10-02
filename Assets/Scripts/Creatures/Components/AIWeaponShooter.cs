1b581a7f45abb23a3461b83c6757c533959b9c4e
Braian Alejandro Ledesma <braian.ledesma-99@hotmail.com>
2019-10-02 03:45:11 -0300

    Add system attack to enemies

---
+++ Assets/Scripts/Creatures/Components/AIWeaponShooter.cs
@@ -0,0 +1,35 @@
+﻿using CreaturesAddons;
+using UnityEngine;
+
+namespace EnemyAddons
+{
+    public class AIWeaponShooter : MonoBehaviour
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Weapons configuration")]
+        private AIWeapons[] weapons;
+#pragma warning restore CS0649
+        
+        void Update()
+        {
+            foreach(AIWeapons weapon in weapons)
+            {
+                if (weapon.ShouldAttack)
+                    weapon.weapon.TryExecute();
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class AIWeapons
+    {
+        [Tooltip("Weapon.")]
+        public Weapon weapon;
+        [Tooltip("Slash.")]
+        public Slash slash;
+
+        public bool ShouldAttack => slash.rayCasting.Raycast(1 << slash.layerToHit);
+    }
+}
+
+
--- Assets/Scripts/Creatures/Components/Weapons/Slash.cs
+++ Assets/Scripts/Creatures/Components/Weapons/Slash.cs
@@ -11,30 +11,50 @@ namespace CreaturesAddons
 
         [Header("Setup")]
 #pragma warning disable CS0649
-        [SerializeField]
-        private RayCasting rayCasting;
-        [SerializeField, Layer]
-        private int layerToHit;
+        //[SerializeField]
+        public RayCasting rayCasting;
+        [Layer]
+        public int layerToHit;
 #pragma warning restore CS0649
 
         private Transform thisTransform;
+        private Animator thisAnimator;
+
+        private static class ANIMATION_STATES
+        {
+            public const string
+                ATTACK = "Attack";
+        }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Type Safety", "UNT0006:Incorrect message signature", Justification = "This isn't Unity method.")]
         public override void Init(Creature creature)
         {
             thisTransform = creature.Transform;
+            thisAnimator = creature.animator;
             base.Init(creature);
         }
 
         protected override void Attack()
         {
-            RaycastHit2D raycastHit = rayCasting.Raycast(1 << layerToHit); // Ignore any layer that isn't layerToHit
-            if (raycastHit.transform == null)
-                return;
+            thisAnimator.SetTrigger(ANIMATION_STATES.ATTACK);
+            //RaycastHit2D raycastHit = rayCasting.Raycast(1 << layerToHit); // Ignore any layer that isn't layerToHit
+            //if (raycastHit.transform == null)
+            //    return;
 
-            Transform victim = raycastHit.transform;
-            victim.GetComponent<ITakeDamage>()?.TakeDamage(damage);
-            victim.GetComponent<IPush>()?.Push(thisTransform.position, pushStrength);
+            //Transform victim = raycastHit.transform;
+            //victim.GetComponent<ITakeDamage>()?.TakeDamage(damage);
+            //victim.GetComponent<IPush>()?.Push(thisTransform.position, pushStrength);
+        }
+
+        void HitTarget()
+        {
+            RaycastHit2D[] raycastHits = rayCasting.RaycastAll(1 << layerToHit); // Ignore any layer that isn't layerToHit
+            for (int n = 0; n < raycastHits.Length; n++)
+            {
+                Transform victim = raycastHits[n].transform;
+                victim.transform.GetComponent<ITakeDamage>()?.TakeDamage(damage);
+                victim.transform.GetComponent<IPush>()?.Push(thisTransform.position, pushStrength);
+            }
         }
     }
 }
