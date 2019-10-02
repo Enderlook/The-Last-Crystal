dc655c1cb5dc763031497a3b49e03690dcf1825a
Enderlook <enderlook@gmail.com>
2019-10-01 21:10:14 -0300

    Add Player respawning system
    Add Player respawning system.
    Add CreatureSpawn, RespawnTracker and InitializePlayers classes.
    Remove Wizard and Warrior from Configuration.

---
+++ Assets/Scripts/Masters/Creature Spawn/InitializePlayers.cs
@@ -0,0 +1,31 @@
+﻿using CreaturesAddons;
+using FloatPool.Decorators;
+using UnityEngine;
+
+public class InitializePlayers : MonoBehaviour
+{
+    [SerializeField, Tooltip("Warrior health bar.")]
+    public HealthBar warriorHealthBar;
+
+    [SerializeField, Tooltip("Wizard health bar.")]
+    public HealthBar wizardHealthBar;
+
+    public void InitializeWarrior(GameObject warrior)
+    {
+        InitializeCreature(warrior, warriorHealthBar);
+        Global.warrior = warrior.transform;
+    }
+
+    public void InitializeWizard(GameObject wizard)
+    {
+        InitializeCreature(wizard, wizardHealthBar);
+        Global.wizard = wizard.transform;
+    }
+
+    private static void InitializeCreature(GameObject creature, HealthBar healthBar)
+    {
+        BarDecorator barDecorator = creature.GetComponent<Creature>().health.GetLayer<BarDecorator>();
+        barDecorator.bar = healthBar;
+        barDecorator.UpdateValues();
+    }
+}
