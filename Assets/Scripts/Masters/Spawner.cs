799dd719f5abfaad16d9cec7315336992b902b16
Enderlook <enderlook@gmail.com>
2019-10-04 00:47:53 -0300

    Turn into private fields in Spawner and use arrow methods

--- Assets/Scripts/Masters/Spawner.cs
+++ Assets/Scripts/Masters/Spawner.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+#pragma warning disable CS0649
     [Header("Setup")]
-    [Tooltip("Enemies to spawn.")]
-    public GameObject[] enemies;
-    [Tooltip("Count of enemies to spawn.")]
-    public int count;
-    [Tooltip("Time between spawn enemies.")]
-    public float timeBtwSpawn;
-    [Tooltip("Start time spawn")]
-    public float startSpawn;
+    [SerializeField, Tooltip("Enemies to spawn.")]
+    private GameObject[] enemies;
+    [SerializeField, Tooltip("Count of enemies to spawn.")]
+    private int count;
+    [SerializeField, Tooltip("Time between spawn enemies.")]
+    private float timeBtwSpawn;
+    [SerializeField, Tooltip("Start time spawn")]
+    private float startSpawn;
+#pragma warning restore CS0649
 
     private int enemiesInGame = 0;
 
     [DrawVectorRelativeToTransform]
     public List<Vector2> points;
 
-    private void Start()
-    {
-        StartCoroutine(SpawnEnemies());
-    }
+    private void Start() => StartCoroutine(SpawnEnemies());
 
     IEnumerator SpawnEnemies()
     {
