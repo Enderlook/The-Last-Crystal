5c1012d2af6b41d905044330903dd3696067aea1
Enderlook <enderlook@gmail.com>
2019-10-19 23:09:21 -0300

    Put StoppableRigidbody in new AdditionalComponents namespace

--- Assets/Scripts/Creatures/Creature.cs
+++ Assets/Scripts/Creatures/Creature.cs
@@ -1,4 +1,5 @@
 using System;
+using AdditionalComponents;
 using CreaturesAddons.Weapons;
 using UnityEngine;
 
--- Assets/Scripts/GUI/Menu.cs
+++ Assets/Scripts/GUI/Menu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AdditionalComponents;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
