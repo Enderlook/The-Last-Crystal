ace8b9d3d288e5f1a8c596f1a7b18045fa72e8c1
Enderlook <enderlook@gmail.com>
2019-10-26 10:08:45 -0300

    Add IndentedAttribute
    Add IndentedAttribute and IndentedDrawer.

---
+++ Assets/Packages/Attributes/Editor Attributes/Others/Editor/IndentedDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AdditionalAttributes
+{
+    [CustomPropertyDrawer(typeof(IndentedAttribute))]
+    internal class IndentedDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            IndentedAttribute indentedAttribute = (IndentedAttribute)attribute;
+            int indentation = indentedAttribute.IndentationOffset;
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.indentLevel += indentation;
+            EditorGUI.PropertyField(position, property, label);
+            EditorGUI.indentLevel -= indentation;
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => EditorGUI.GetPropertyHeight(property, label);
+    }
+}
