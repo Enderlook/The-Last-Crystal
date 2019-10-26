c86dc708af9753ba75f87d372e9aa45da797fc79
Braian996 <braian.ledesma-99@hotmail.com>
2019-10-26 05:20:11 -0300

    Add dash effect to warrior

---
+++ Assets/Scripts/Creatures/Components/Players/DashEffect.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using CreaturesAddons;
+using Utils;
+using System.Collections;
+
+namespace PlayerAddons
+{
+    public enum DashState { Start, Dashing, Cooldown }
+
+    public class DashEffect : MonoBehaviour, IInit, IUpdate
+    {
+        [Header("Setup")]
+        [SerializeField, Tooltip("Input for dash.")]
+        private KeyCode dashKey;
+        [SerializeField, Tooltip("Duration of effect.")]
+        private float dashTime = 1;
+        [SerializeField, Tooltip("Velocity dash.")]
+        private float forceDash;
+
+        public DashState dashState;
+
+        private Rigidbody2D rb2D;
+        private SpriteRenderer sprite;
+        private Vector2 savedVelocity;
+        private float maxDash = 20f;
+
+        void IInit.Init(Creature creature)
+        {
+            rb2D = creature.thisRigidbody2D;
+            sprite = creature.sprite;
+        }
+
+        void IUpdate.UpdateBehaviour(float deltaTime)
+        {
+            switch (dashState)
+            {
+                case DashState.Start:
+                    if (Input.GetKey(dashKey))
+                    {
+                        savedVelocity = rb2D.velocity;
+                        float vel = sprite.flipX ? forceDash : -forceDash;
+                        rb2D.velocity = Vector2.right * vel;
+                        dashState = DashState.Dashing;
+                    }
+                    break;
+                case DashState.Dashing:
+                    dashTime += deltaTime * 3;
+                    if (dashTime >= maxDash)
+                    {
+                        dashTime = maxDash;
+                        rb2D.velocity = savedVelocity;
+                        dashState = DashState.Cooldown;
+                    }
+                    break;
+                case DashState.Cooldown:
+                    dashTime -= deltaTime;
+                    if (dashTime <= 0)
+                    {
+                        dashTime = 0;
+                        dashState = DashState.Start;
+                    }
+                    break;
+            }
+        }
+
+        IEnumerator DashMove()
+        {
+            if (sprite.flipX) rb2D.AddForce(rb2D.transform.right * forceDash);
+            else rb2D.AddForce(-rb2D.transform.right * forceDash);
+            yield return new WaitForSeconds(.3f);
+        }
+    }
+}
