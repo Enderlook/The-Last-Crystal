43bca2ad599581550e100f533cdbe07a3cedae26
Enderlook <enderlook@gmail.com>
2019-10-26 11:22:36 -0300

    Add RangeWithSwitch
    Add RangeFloatSwitchable, RangeIntSwitchable, RangeFloatStepSwitchable, RangeintStepSwitchable classes.
    Refactor range interfaces

---
+++ Assets/Packages/Serializables/Range/RangeWithSwitch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Serializables;
+
+namespace Range
+{
+    [Serializable]
+    public class RangeFloatSwitchable : Switch<RangeFloat, float>, IBasicRange<float>, IBasicRangeInt<float>
+    {
+        public float Value => Alternative ? Item2 : Item1.Value;
+        public int ValueInt => Alternative ? RangeFloat.FloatToIntByChance(Item2) : Item1.ValueInt;
+
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// </summary>
+        /// <param name="source"><see cref="RangeFloatSwitchable"/> instance used to determine the random float.</param>
+        public static explicit operator float(RangeFloatSwitchable source) => source.Value;
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// The result is always an integer. Decimal numbers are used as chance to increment by one.
+        /// </summary>
+        /// <param name="source"><see cref="RangeFloatSwitchable"/> instance used to determine the random int.</param>
+        public static explicit operator int(RangeFloatSwitchable source) => source.ValueInt;
+    }
+
+    [Serializable]
+    public class RangeFloatStepSwitchable : Switch<RangeFloatStep, float>, IBasicRange<float>, IBasicRangeInt<float>
+    {
+        public float Value => Alternative ? Item2 : Item1.Value;
+        public int ValueInt => Alternative ? RangeFloat.FloatToIntByChance(Item2) : Item1.ValueInt;
+
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// </summary>
+        /// <param name="source"><see cref="RangeFloatSwitchable"/> instance used to determine the random float.</param>
+        public static explicit operator float(RangeFloatStepSwitchable source) => source.Value;
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// The result is always an integer. Decimal numbers are used as chance to increment by one.
+        /// </summary>
+        /// <param name="source"><see cref="RangeFloatSwitchable"/> instance used to determine the random int.</param>
+        public static explicit operator int(RangeFloatStepSwitchable source) => source.ValueInt;
+    }
+
+    [Serializable]
+    public class RangeIntSwitchable : Switch<RangeInt, int>, IBasicRangeInt<int>
+    {
+        public int Value => Alternative ? Item2 : Item1.Value;
+        public int ValueInt => Value;
+
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// </summary>
+        /// <param name="source"><see cref="RangeIntSwitchable"/> instance used to determine the random int.</param>
+        public static explicit operator int(RangeIntSwitchable source) => source.Value;
+    }
+
+    [Serializable]
+    public class RangeIntStepSwitchable : Switch<RangeIntStep, int>, IBasicRangeInt<int>
+    {
+        public int Value => Alternative ? Item2 : Item1.Value;
+        public int ValueInt => Value;
+
+        /// <summary>
+        /// Return a random value between <see cref="Min"/> and <see cref="Max"/>.
+        /// </summary>
+        /// <param name="source"><see cref="RangeIntStepSwitchable"/> instance used to determine the random int.</param>
+        public static explicit operator int(RangeIntStepSwitchable source) => source.Value;
+    }
+}
