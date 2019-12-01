e43bc9eb2359111ca681707c7411808ad9d87eae
Enderlook <enderlook@gmail.com>
2019-12-01 15:09:33 -0300

    .

---
+++ Assets/Scripts/Capturer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Capturer : MonoBehaviour
+{
+    [SerializeField, Tooltip("Screenshoot key.")]
+    private KeyCode capture;
+
+    private void Start() => Screen.SetResolution(1920, 1080, true);
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(capture))
+            ScreenCapture.CaptureScreenshot(Time.frameCount.ToString()+".png", 4);
+    }
+}
---
+++ Assets/Scripts/PoolTester.cs
@@ -0,0 +1,41 @@
+﻿using FloatPool;
+using UnityEngine;
+
+public class PoolTester : MonoBehaviour
+{
+    [SerializeField, Tooltip("FloatPool to affect.")]
+    private Pool pool;
+
+    [SerializeField, Tooltip("Key to decrease.")]
+    private KeyCode decreaseKey;
+
+    [SerializeField, Tooltip("Amount to decrease.")]
+    private float decreaseAmount;
+
+    [SerializeField, Tooltip("Key to increase.")]
+    private KeyCode increaseKey;
+
+    [SerializeField, Tooltip("Amount to increase.")]
+    private float increaseAmount;
+
+    [SerializeField, Tooltip("Initialize pool.")]
+    private bool initialize;
+    [SerializeField, Tooltip("Include pool to update in each frame.")]
+    private bool update;
+
+    private void Start()
+    {
+        if (initialize)
+            pool.Initialize();
+    }
+
+    private void Update()
+    {
+        if (update)
+            pool.UpdateBehaviour(Time.deltaTime);
+        if (Input.GetKeyDown(decreaseKey))
+            pool.Decrease(decreaseAmount);
+        if (Input.GetKeyDown(increaseKey))
+            pool.Increase(increaseAmount);
+    }
+}
