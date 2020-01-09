0c0d4a36c9b7e73f60d0928f205fd98357878d55
Enderlook <enderlook@gmail.com>
2020-01-09 17:18:50 -0300

    Fix documentation for post compiling

--- Assets/Packages/Attributes/Editor Attributes/Post Compiling/Editor/Attributes/Execute/ExecuteOnEachMethodOfEachTypeWhenScriptsReloads.cs
+++ Assets/Packages/Attributes/Editor Attributes/Post Compiling/Editor/Attributes/Execute/ExecuteOnEachMethodOfEachTypeWhenScriptsReloads.cs
@@ -8,7 +8,7 @@ namespace AdditionalAttributes.PostCompiling
 {
     /// <summary>
     /// Executes the method decorated by this attribute for each method on each <see cref="Type"/> compiled by Unity each time Unity compiles code.<br>
-    /// The method to decorate must have the signature DoSomething(<see cref="Sytem.Reflection.MethodInfo"/>).
+    /// The method to decorate must have the signature DoSomething(<see cref="MethodInfo"/>).
     /// </summary>
     [AttributeUsageAccessibility(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public)]
     [AttributeUsageMethod(1, typeof(MethodInfo))]
@@ -17,7 +17,7 @@ namespace AdditionalAttributes.PostCompiling
     {
         /// <summary>
         /// Executes the method decorated by this attribute for each method on each <see cref="Type"/> compiled by Unity.<br>
-        /// The method to decorate must have the signature DoSomething(<see cref="Sytem.Reflection.MethodInfo"/>).
+        /// The method to decorate must have the signature DoSomething(<see cref="MethodInfo"/>).
         /// </summary>
         /// <param name="loop">In which loop of the execution will this script execute.</param>
         public ExecuteOnEachMethodOfEachTypeWhenScriptsReloads(int loop = 0) : base(loop) { }
