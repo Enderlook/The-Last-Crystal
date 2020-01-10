03125f13d905be222f206e121f55ff0f139aaf76
Enderlook <enderlook@gmail.com>
2020-01-10 09:41:46 -0300

    Add SoundPlay

---
+++ Assets/Packages/Components/ScriptableSound/SoundPlay.cs
@@ -0,0 +1,26 @@
+﻿using AdditionalAttributes;
+
+using System;
+
+using UnityEngine;
+
+namespace ScriptableSound
+{
+    [Serializable]
+    public class SoundPlay
+    {
+        [SerializeField, Tooltip("AudioSource where sound is played.")]
+        private AudioSource audioSource;
+
+        [SerializeField, Tooltip("Sound to play."), Expandable]
+        private Sound sound;
+
+        public void Update() => sound.Update();
+
+        public void Play(Action endCallback = null)
+        {
+            sound.SetConfiguration(new SoundConfiguration(audioSource, endCallback));
+            sound.Play();
+        }
+    }
+}
