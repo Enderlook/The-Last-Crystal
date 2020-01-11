d64e259982a081688af5dad8fbba5c205f3f56a9
Enderlook <enderlook@gmail.com>
2020-01-11 11:54:49 -0300

    Supress message IDE0051 in CreatureSpawn

--- Assets/Scripts/Masters/Creature Spawn/CreatureSpawn.cs
+++ Assets/Scripts/Masters/Creature Spawn/CreatureSpawn.cs
@@ -22,7 +22,10 @@ public class CreatureSpawn : MonoBehaviour
     private NavigationGraph navigationGraph;
 #pragma warning restore CS0649
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
     private void Start() => Array.ForEach(creatures, e => e.Initialize(navigationGraph));
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
     private void Update() => Array.ForEach(creatures, e => e.Update(Time.deltaTime));
 
     [Serializable]
