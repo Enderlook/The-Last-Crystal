d07db59feaa24264274afc30513ec1c3da29f4f3
Enderlook <enderlook@gmail.com>
2020-01-11 15:59:17 -0300

    Disable CS0649 warning in Kamikaze

--- Assets/Scripts/Creatures/Components/Weapons/Kamikaze.cs
+++ Assets/Scripts/Creatures/Components/Weapons/Kamikaze.cs
@@ -6,8 +6,10 @@ namespace CreaturesAddons.Weapons
 {
     public class Kamikaze : PassiveMelee
     {
+#pragma warning disable CS0649
         [SerializeField, Tooltip("Sound played on hit.")]
         private Sound hitSound;
+#pragma warning restore CS0649
 
         public override void ProduceDamage(object victim)
         {
