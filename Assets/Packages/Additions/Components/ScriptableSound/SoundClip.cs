ae4db8fa215b27454a12b0d70354f73dcef3f2af
Enderlook <enderlook@gmail.com>
2020-01-29 13:27:08 -0300

    Add Win, Lose and Menu music
    Add Win, Lose and Menu music.
    Modify Menu to use ScriptableSound instead of SoundSystem.

--- Assets/Packages/Additions/Components/ScriptableSound/SoundClip.cs
+++ Assets/Packages/Additions/Components/ScriptableSound/SoundClip.cs
@@ -83,10 +83,12 @@ namespace Additions.Components.ScriptableSound
         }
 
 #if UNITY_EDITOR
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void OnValidate()
         {
             foreach (SoundModifier modifier in modifiers)
-                modifier.Validate(this);
+                if (modifier != null)
+                    modifier.Validate(this);
         }
 #endif
     }
