b0a961f7c4e5ecd9f56f8553f437db998910bfb0
Enderlook <enderlook@gmail.com>
2020-02-01 11:18:18 -0300

    Add duration field to SoundClipEditor, rework PlayAudioClipDrawer and add AudioUtil

--- Assets/Packages/Additions/Attributes/Editor Attributes/Editor/PlayAudioClipDrawer.cs
+++ Assets/Packages/Additions/Attributes/Editor Attributes/Editor/PlayAudioClipDrawer.cs
@@ -1,7 +1,7 @@
-﻿using Additions.Utils.UnityEditor;
+﻿using Additions.Utils;
+using Additions.Utils.UnityEditor;
 
 using System;
-using System.Reflection;
 
 using UnityEditor;
 
@@ -12,25 +12,6 @@ namespace Additions.Attributes
     [CustomPropertyDrawer(typeof(PlayAudioClipAttribute))]
     public class PlayAudioClipDrawer : AdditionalPropertyDrawer
     {
-        // https://forum.unity.com/threads/way-to-play-audio-in-editor-using-an-editor-script.132042/
-        // https://github.com/MattRix/UnityDecompiled/blob/cc432a3de42b53920d5d5dae85968ff993f4ec0e/UnityEditor/UnityEditor/AudioUtil.cs
-
-        private static readonly Action<AudioClip> Play;
-        private static readonly Action<AudioClip> Stop;
-        private static readonly Func<AudioClip, bool> IsClipPlaying;
-        private static readonly Func<AudioClip, float> GetClipPosition;
-
-        static PlayAudioClipDrawer()
-        {
-            Type audioUtilClass = typeof(AudioImporter).Assembly.GetType("UnityEditor.AudioUtil");
-            Type[] parameter = new Type[] { typeof(AudioClip) };
-            const BindingFlags bindingFlags = BindingFlags.Static | BindingFlags.Public;
-            Play = (Action<AudioClip>)audioUtilClass.GetMethod("PlayClip", bindingFlags, null, parameter, null).CreateDelegate(typeof(Action<AudioClip>));
-            Stop = (Action<AudioClip>)audioUtilClass.GetMethod("StopClip", bindingFlags, null, parameter, null).CreateDelegate(typeof(Action<AudioClip>));
-            IsClipPlaying = (Func<AudioClip, bool>)audioUtilClass.GetMethod("IsClipPlaying", bindingFlags, null, parameter, null).CreateDelegate(typeof(Func<AudioClip, bool>));
-            GetClipPosition = (Func<AudioClip, float>)audioUtilClass.GetMethod("GetClipPosition", bindingFlags, null, parameter, null).CreateDelegate(typeof(Func<AudioClip, float>));
-        }
-
         protected override void OnGUIAdditional(Rect position, SerializedProperty property, GUIContent label)
         {
             PlayAudioClipAttribute playAudioClipAttribute = (PlayAudioClipAttribute)attribute;
@@ -40,9 +21,14 @@ namespace Additions.Attributes
                 EditorGUI.PropertyField(position, property, label);
             else
             {
-                bool isPlaying = IsClipPlaying(audioClip);
-                GUIContent playGUIContent = isPlaying ? EditorGUIUtility.IconContent("PauseButton", $"Stop {typeof(AudioClip)}. Remaining duration: {audioClip.length - GetClipPosition(audioClip)} seconds.") : EditorGUIUtility.IconContent("PlayButton", $"Play {typeof(AudioClip)}. Duration: {audioClip.length} seconds.");
+                bool isPlaying = AudioUtil.IsClipPlaying(audioClip);
+                GUIContent playGUIContent = EditorGUIUtility.IconContent(isPlaying ? "PauseButton" : "PlayButton");
                 float width = GUI.skin.button.CalcSize(playGUIContent).x;
+
+                playGUIContent.tooltip = TimeSpan.FromSeconds(isPlaying
+                    ? audioClip.length - AudioUtil.GetClipPosition(audioClip)
+                    : audioClip.length
+                ).ToString(@"hh\:mm\:ss\:ff");
 
                 EditorGUI.PropertyField(new Rect(position.x, position.y, position.width - width, position.height), property, label, true);
 
@@ -50,16 +36,16 @@ namespace Additions.Attributes
 
                 if (GUI.Button(new Rect(position.x + position.width - width, position.y, width, position.height / (showProgressBar ? 2 : 1)), playGUIContent))
                     if (isPlaying)
-                        Stop(audioClip);
+                        AudioUtil.StopClip(audioClip);
                     else
-                        Play(audioClip);
+                        AudioUtil.PlayClip(audioClip);
 
                 if (showProgressBar)
-                {
-                    EditorGUI.ProgressBar(new Rect(position.x + position.width - width, position.y + position.height / 2, width, position.height / 2), GetClipPosition(audioClip) / audioClip.length, "");
+                    EditorGUI.ProgressBar(new Rect(position.x + position.width - width, position.y + position.height / 2, width, position.height / 2), AudioUtil.GetClipPosition(audioClip) / audioClip.length, "");
+
+                if (isPlaying)
                     // Forces repaint all the inspector per frame
                     EditorUtility.SetDirty(property.serializedObject.targetObject);
-                }
             }
         }
     }
--- Assets/Packages/Additions/Components/ScriptableSound/Editor/SoundClipEditor.cs
+++ Assets/Packages/Additions/Components/ScriptableSound/Editor/SoundClipEditor.cs
@@ -1,7 +1,12 @@
 ﻿using Additions.Utils.UnityEditor;
 
+using System;
+
 using UnityEditor;
 
+using UnityEngine;
+
+using UnityObject = UnityEngine.Object;
 namespace Additions.Components.ScriptableSound
 {
     [CustomEditor(typeof(SoundClip))]
@@ -13,7 +18,15 @@ namespace Additions.Components.ScriptableSound
 
             EditorGUI.BeginChangeCheck();
 
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("audioClip"), true);
+            SerializedProperty audioClipProperty = serializedObject.FindProperty("audioClip");
+            EditorGUILayout.PropertyField(audioClipProperty, true);
+            UnityObject audioClip = audioClipProperty.objectReferenceValue;
+            if (audioClip != null)
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUILayout.TextField(new GUIContent("Duration", "Duration of the clip."), TimeSpan.FromSeconds(((AudioClip)audioClip).length).ToString(@"hh\:mm\:ss\:ff"));
+                EditorGUI.EndDisabledGroup();
+            }
             EditorGUILayout.PropertyField(serializedObject.FindProperty("modifiers"), true);
             SoundPlayerEditor.DrawAmountField(serializedObject.FindProperty("playsAmount"));
 
