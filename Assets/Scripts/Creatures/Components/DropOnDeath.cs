11e9c5c3a5668bc4d00abab2bd600552c5aef83c
Enderlook <enderlook@gmail.com>
2020-02-08 17:46:37 -0300

    Add explosion animation when Tiny Bomb commits suicide
    Add explosion animation when Tiny Bomb commits suicide.
    Add DropOnDeath.deathCondition field.
    Add DestroyWhenAnimationEnds.
    Reorganice, remove and updates images of Tiny Bomb

--- Assets/Scripts/Creatures/Components/DropOnDeath.cs
+++ Assets/Scripts/Creatures/Components/DropOnDeath.cs
@@ -1,17 +1,30 @@
 ﻿using Additions.Serializables;
-
+using System;
 using UnityEngine;
 
 namespace Creatures
 {
     public class DropOnDeath : MonoBehaviour, IDie
     {
+        [Flags]
+        public enum DeathCondition
+        {
+            Always = WhenMurdered | WhenSuicided,
+            WhenSuicided = 1 << 2,
+            WhenMurdered = 1 << 1
+        }
+
         [SerializeField, Tooltip("Possible GameObjects to drop.")]
         private WeightedGameObjects dropsTable;
 
+        [SerializeField, Tooltip("In which conditions it should drop")]
+        private DeathCondition deathCondition = DeathCondition.Always;
+
         void IDie.Die(bool suicide)
         {
-            if (dropsTable.TryGetRandomElement(out GameObject result))
+            if ((((deathCondition & DeathCondition.WhenSuicided) != 0 && suicide)
+                || ((deathCondition & DeathCondition.WhenMurdered) != 0 && !suicide))
+                && dropsTable.TryGetRandomElement(out GameObject result))
                 Instantiate(result, transform.position, Quaternion.identity);
         }
     }
