4bf436525284a4877bd24bcb9aafac44a3d566b9
Enderlook <enderlook@gmail.com>
2020-02-12 23:10:56 -0300

    Add layer between Atom and AtomGetter
    Rename AtomGetter to AtomGetterWrapper.
    Add AtomGetter.

---
+++ Assets/Packages/Additions/Serializables/Atoms/Bases/AtomGetterWrapper`2.cs
@@ -0,0 +1,10 @@
+﻿namespace Additions.Serializables.Atoms
+{
+    public abstract class AtomGetterWrapper<T, U> : AtomGetter<T, U> where T : IGet<U>
+    {
+        /// <summary>
+        /// <see cref="value"/> as property.
+        /// </summary>
+        public override U Value => value.Value;
+    }
+}
--- Assets/Packages/Additions/Serializables/Atoms/Bases/AtomGetter`2.cs
+++ Assets/Packages/Additions/Serializables/Atoms/Bases/AtomGetter`2.cs
@@ -2,19 +2,16 @@
 
 namespace Additions.Serializables.Atoms
 {
-    public abstract class AtomGetter<T, U> : Atom, IGet<U> where T : IGet<U>
+    public abstract class AtomGetter<T, U> : Atom
     {
         [SerializeField]
-        private T value;
+        protected T value;
+
+        public abstract U Value { get; }
 
         /// <summary>
         /// Boxed value of <see cref="Value"/>
         /// </summary>
         public object ObjectValue => Value;
-
-        /// <summary>
-        /// <see cref="value"/> as property.
-        /// </summary>
-        public U Value => value.Value;
     }
 }
