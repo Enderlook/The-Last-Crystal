ae41661f10b9193e509c0d48369c13fe762626de
Enderlook <enderlook@gmail.com>
2020-02-14 00:18:28 -0300

    Add supress message in ScriptableObjectWindow

--- Assets/Packages/Additions/Attributes/Editor Attributes/Editor/ScriptableObjectWindow.cs
+++ Assets/Packages/Additions/Attributes/Editor Attributes/Editor/ScriptableObjectWindow.cs
@@ -113,6 +113,7 @@ namespace Additions.Attributes
             window.property = property;
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void OnGUI()
         {
             titleContent = new GUIContent("Scriptable Object Manager");
