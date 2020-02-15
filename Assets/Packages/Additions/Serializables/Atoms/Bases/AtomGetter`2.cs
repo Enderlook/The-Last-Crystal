80d322fb9ff1054bade2c6e8738b56a49d01ac87
Enderlook <enderlook@gmail.com>
2020-02-15 12:44:42 -0300

    Add ExpandableAttribute to AtomGetter<T, U>

--- Assets/Packages/Additions/Serializables/Atoms/Bases/AtomGetter`2.cs
+++ Assets/Packages/Additions/Serializables/Atoms/Bases/AtomGetter`2.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using Additions.Attributes;
+using Additions.Attributes.AttributeUsage;
+using UnityEngine;
 
 namespace Additions.Serializables.Atoms
 {
     public abstract class AtomGetter<T, U> : AtomGet<U>
     {
-        [SerializeField]
+        // Don't check because generic types can't be serialized by Unity, but this class is just a template
+        [DoNotCheck(typeof(ExpandableAttribute))]
+        [SerializeField, Expandable]
         protected T value;
     }
 }
