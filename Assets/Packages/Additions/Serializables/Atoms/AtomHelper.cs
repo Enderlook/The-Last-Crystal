3a71bc0fc6e6501925f2ccfb857b8eadd1eacb4b
Enderlook <enderlook@gmail.com>
2020-02-16 11:40:18 -0300

    Change AtomHelper.GetValue<T> to cast to IGet<T> instead of AtomGet<T>

--- Assets/Packages/Additions/Serializables/Atoms/AtomHelper.cs
+++ Assets/Packages/Additions/Serializables/Atoms/AtomHelper.cs
@@ -3,11 +3,11 @@
     public static class AtomHelper
     {
         /// <summary>
-        /// Get value of the atom. Only works in atoms derived from <see cref="AtomGet{T}"/>.
+        /// Get value of the atom. Only works in atoms derived from <see cref="IGet{T}"/>.
         /// </summary>
         /// <typeparam name="T">Type of value to get.</typeparam>
         /// <param name="source">Atom where the value is get.</param>
         /// <returns>Value of the atom.</returns>
-        public static T GetValue<T>(this Atom source) => ((AtomGet<T>)source).Value;
+        public static T GetValue<T>(this Atom source) => ((IGet<T>)source).Value;
     }
 }
