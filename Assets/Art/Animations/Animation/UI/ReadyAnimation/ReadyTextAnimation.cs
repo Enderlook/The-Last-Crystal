203c0740083b35757f03f11af2db1f63541e3ed0
Braian996 <braian.ledesma-99@hotmail.com>
2020-02-26 05:23:44 -0300

    Add tutorial in Level_1

--- Assets/Art/Animations/Animation/UI/ReadyAnimation/ReadyTextAnimation.cs
+++ Assets/Art/Animations/Animation/UI/ReadyAnimation/ReadyTextAnimation.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ReadyTextAnimation : MonoBehaviour
 {
+    [Header("Setup")]
+    [SerializeField, Tooltip("True if you have a tutorial, False if you don't have a tutorial.")]
+    private bool tutorial;
+
     public void InitializeEvent()
     {
+        if (tutorial) TutorialManager.TutorialInstance.ActivateChildsObject();
         Spawner.Instance.InitializeWave();
     }
 }
---
+++ Assets/Scripts/Masters/TutorialManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    
+    [Header("Setup")]
+
+    [SerializeField, Tooltip("Toggles object.")]
+    private TogglesUI[] toggles;
+
+    [SerializeField, Tooltip("Child objects.")]
+    private GameObject[] child;
+
+    private bool isActiveTutorial = false;
+
+    private static TutorialManager tutorialInstance;
+
+    public static TutorialManager TutorialInstance
+    {
+        get
+        {
+            if (tutorialInstance == null)
+            {
+                GameObject tutorialManager = new GameObject();
+            }
+
+            return tutorialInstance;
+        }
+    }
+
+    private void Awake() => tutorialInstance = this;
+
+    private void Start() => InitializedToggles();
+
+    private void Update()
+    {
+        if (isActiveTutorial)
+            SetToggleByKeyCode();
+    }
+
+    private void SetToggleByKeyCode()
+    {
+        if (VerifyToggles())
+        {
+            DeactivateChildsObject();
+            return;
+        }
+        for (int x = 0; x < toggles.Length; x++)
+        {
+            if (toggles[x].mouseButtons == TogglesUI.MouseButton.None)
+            {
+                for (int y = 0; y < toggles[x].keyCodes.Length; y++)
+                {
+                    if (Input.GetKeyDown(toggles[x].keyCodes[y]))
+                    {
+                        toggles[x].toggle.isOn = true;
+                    }
+                }
+            }
+            else if (toggles[x].mouseButtons != TogglesUI.MouseButton.None && 
+                Input.GetMouseButtonDown((int)toggles[x].mouseButtons))
+            {
+                toggles[x].toggle.isOn = true;
+            }
+        }
+    }
+
+    private bool VerifyToggles()
+    {
+        bool isToggle = true;
+
+        int x = 0;
+        while (isToggle && x < toggles.Length)
+        {
+            if (toggles[x].toggle.isOn)
+            {
+                isToggle = true;
+                x++;
+            }
+            else
+                isToggle = false;
+        }
+
+        return isToggle ? true : false;
+    }
+
+    private void InitializedToggles()
+    {
+        for (int x = 0; x < toggles.Length; x++)
+        {
+            toggles[x].toggle.isOn = false;
+        }
+    }
+
+    public void ActivateChildsObject()
+    {
+        isActiveTutorial = true;
+        for (int i = 0; i < child.Length; i++)
+        {
+            child[i].SetActive(true);
+        }
+    }
+
+    private void DeactivateChildsObject()
+    {
+        for (int i = 0; i < child.Length; i++)
+        {
+            child[i].SetActive(false);
+        }
+    }
+}
+
+[System.Serializable]
+public class TogglesUI
+{
+    public enum MouseButton { None = -1, Left = 0, Right = 1, Middle = 2 }
+
+    [Tooltip("Toggle object.")]
+    public Toggle toggle;
+
+    [Tooltip("Key action player.")]
+    public KeyCode[] keyCodes;
+
+    [Tooltip("Mouse action player.")]
+    public MouseButton mouseButtons;
+}
