56c7594a76f0259ae95c8068bd121865c87326d8
Braian996 <braian.ledesma-99@hotmail.com>
2020-03-03 06:50:51 -0300

    Add Introduction

---
+++ Assets/Art/Animations/Animation/Introduction/IntroductionAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroductionAnimation : MonoBehaviour
+{
+    [Header("Setup")]
+    [SerializeField, Tooltip("Animator of the game object.")]
+    private Animator animator;
+
+    [SerializeField, Tooltip("State animation.")]
+    private string state;
+
+    [SerializeField, Tooltip("Script of menu.")]
+    private Menu menu;
+
+    [SerializeField, Tooltip("Level to go.")]
+    private string levelName;
+
+    public void PlayAnimation()
+    {
+        animator.SetBool(state, true);
+    }
+
+    public void GoToLevel()
+    {
+        animator.SetBool(state, false);
+        menu.LoadScene(levelName);
+    }
+}
